5e6e5ffd0cbea3de13b4b8aa38efe6d086ba69c9
Koemseang <koemseang.phoeurn@student.passerellesnumeriques.org>
2026-07-12 15:16:53 +0700

    update products with id

--- src/api.js
+++ src/api.js
@@ -12,15 +12,15 @@ export default {
     return api.get('/products');
   },
   getProduct(id) {
-    return api.get(`/products/${id}`);
+    return api.get('/products/:id');
   },
   createProduct(data) {
     return api.post('/products', data);
   },
   updateProduct(id, data) {
-    return api.put(`/products/${id}`, data);
+    return api.put('/products/:id', data);
   },
   deleteProduct(id) {
-    return api.delete(`/products/${id}`);
+    return api.delete('/products/:id');
   }
 };
